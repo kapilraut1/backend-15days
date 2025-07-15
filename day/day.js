ee4331c2852529cf1f06de1a82e28ab0c0a8858b
Kapil Raut <kapilraut026@gmail.com>
2025-07-15 22:06:11 +0545

    dhha

--- day/day.js
+++ day/day.js
@@ -2,9 +2,18 @@ const { Console } = require('console');
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-    console.log(res);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<title><h1>Hello</h1></title>');
+ res.write('<body><h>2Hello</h2></body>');
+res.write('</html>');
+res.end();
 })
+
+
+
+
 const PORT= 3000;
 server.listen(PORT,()=>{
-    console.log(`server listening at port ${PORT}`)
+    console.log(`server listening at port http://localhost:${PORT}`)
 } );
