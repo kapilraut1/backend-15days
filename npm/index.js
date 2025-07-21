a890bf0f4983f7146e9fe9f14f50fb4f00ce44f6
Kapil Raut <kapilraut026@gmail.com>
2025-07-21 18:55:59 +0545

    day 51

---
+++ npm/index.js
@@ -0,0 +1,16 @@
+function calculateArea(width, height){
+    return width* height;
+}
+
+let width = 10,  height= 5;
+
+if(area >100){
+    console.log("Large area");
+}
+else{
+    console.log("Small area");
+}
+
+if(width +height >100){
+    console.log("area is greater than or equal to 100");
+}
