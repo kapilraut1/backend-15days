d72cfc7238e0bd520ccab67c86237b19267ec53f
Kapil Raut <kapilraut026@gmail.com>
2025-07-30 21:57:23 +0545

    day 60

--- npm/models/homes.js
+++ npm/models/homes.js
@@ -1,32 +1,34 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 const rootDir = require("../utils/pathUtil");
 
-let registeredAddress = [];
+let registeredAddress=[];
 
-module.exports = class Homes {
-  constructor(name) {
-    this.name = name;
-  }
+module.exports = class Homes{
+    constructor(name){
+this.name = name;
+    }
 
-  save() {
-    registeredAddress.push(this.name);
-    const filePath = path.join(rootDir, "data", "homes.json");
-    fs.writeFile(filePath, JSON.stringify(registeredAddress), (err) => {
-      console.log(err);
-    });
-  }
 
-  static fetchAll() {
-    const homePath = path.join(rootDir, "data", "homes.json");
-    fs.readFile(homePath, (err, data) => {
-      if (err) {
-        console.log(err);
+    save(){
+        registeredAddress.push(this.name);
+        const filePath= path.join(rootDir, 'data', 'homes.json');
+        fs.writeFile(filePath, JSON.stringify(registeredAddress), (err)=>{
+            console.log(err);
+        })
+    }
+
+    static fetchAll(){
+        const homePath = path.join(rootDir, 'data', 'homes.json');
+        fs.readFile(homePath, (err, data)=>{
+            if(err){
+                console.log(err);
+                return registeredAddress;
+            }
+            registeredAddress = JSON.parse(data);
+        })
+
         return registeredAddress;
-      }
-      registeredAddress = JSON.parse(data);
-    });
+    }
+}
 
-    return registeredAddress;
-  }
-};
