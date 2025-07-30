fe961e69df744683fd4757ec113c5cfae43a40a1
Kapil Raut <kapilraut026@gmail.com>
2025-07-30 21:34:07 +0545

    day 60

--- npm/models/homes.js
+++ npm/models/homes.js
@@ -1,19 +1,32 @@
+const fs = require("fs");
+const path = require("path");
+const rootDir = require("../utils/pathUtil");
 
-let registeredAddress=[];
+let registeredAddress = [];
 
-module.exports = class Homes{
-    constructor(name){
-this.name = name;
-    }
+module.exports = class Homes {
+  constructor(name) {
+    this.name = name;
+  }
 
+  save() {
+    registeredAddress.push(this.name);
+    const filePath = path.join(rootDir, "data", "homes.json");
+    fs.writeFile(filePath, JSON.stringify(registeredAddress), (err) => {
+      console.log(err);
+    });
+  }
 
-    save(){
-        registeredAddress.push(this.name);
-    }
-
-    static fetchAll(){
-        console.log("fetching all homes", registeredAddress)
+  static fetchAll() {
+    const homePath = path.join(rootDir, "data", "homes.json");
+    fs.readFile(homePath, (err, data) => {
+      if (err) {
+        console.log(err);
         return registeredAddress;
-    }
-}
+      }
+      registeredAddress = JSON.parse(data);
+    });
 
+    return registeredAddress;
+  }
+};
